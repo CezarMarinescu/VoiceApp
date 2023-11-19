f06e8585ef68ebd2f97f711874497c4d884b30b2
Razvan <razvanmarin777@gmail.com>
2023-11-19 21:59:36 +0200

    changes to set reminder ( test )

--- script.js
+++ script.js
@@ -1,14 +1,91 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
 
-context.fillStyle = 'rgba(0, 0, 0, 0.8)';
-context.fillRect(0, 0, canvas.width, canvas.height);
+
+function populateCanvas(callback) {
+
+    console.log("Populating canvas...");
+    drawCalendar();
+
+    callback();
+}
+function drawCalendar() {
+    console.log("Drawing calendar...");
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const cellWidth = 100;
+    const cellHeight = 100; 
+    const numRows = 5; 
+    const numColumns = 7;
+    const timeSectionWidth = 60; 
+    const totalWidth = numColumns * cellWidth + timeSectionWidth;
+
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    canvas.width = totalWidth; 
+
+    context.fillStyle = 'black';
+    context.font = '16px Arial'; 
+
+    for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numColumns; col++) {
+            const x = col * cellWidth;
+            const y = row * cellHeight;
+
+            context.strokeRect(x, y, cellWidth, cellHeight); 
+
+            if (row === 0) {
+
+                context.fillText(days[col], x + 10, y + 20);
+            } else {
+
+                const date = col + (row - 1) * numColumns;
+                context.fillText(date, x + 10, y + 20);
+            }
+        }
+    }
+
+    context.fillStyle = 'gray';
+    context.fillRect(numColumns * cellWidth, 0, timeSectionWidth, numRows * cellHeight);
+
+
+    context.fillStyle = 'white';
+    context.font = '10px Arial'; 
+    for (let i = 0; i < 24; i++) {
+        const hourText = (i < 10) ? '0' + i + ':00' : i + ':00';
+        context.fillText(hourText, numColumns * cellWidth + 5, i * (cellHeight / 2) + 15);
+    }
+
+
+
+
+    canvas.addEventListener('click', function(event) {
+        const rect = canvas.getBoundingClientRect();
+        const mouseX = event.clientX - rect.left;
+        const mouseY = event.clientY - rect.top;
+
+        if (mouseX < numColumns * cellWidth && mouseY < numRows * cellHeight) {
+            const clickedCol = Math.floor(mouseX / cellWidth);
+            const clickedRow = Math.floor(mouseY / cellHeight);
+
+            if (clickedRow !== 0) {
+                const selectedDate = clickedCol + (clickedRow - 1) * numColumns + 1; 
+                console.log('Selected Date:', selectedDate);
+            }
+        }
+        if (mouseX >= numColumns * cellWidth) {
+            const clickedHour = Math.floor((mouseY * 24) / (numRows * cellHeight)); 
+            console.log('Selected Hour:', clickedHour);
+        }
+    });
+}
+
+
 
 let audio;
 
-//PLAY MUSIC and Reminders
+// PLAY MUSIC and Reminders
 
-function setReminder(task, time) {
+function setReminder(task, time, selectedSound) {
     const now = new Date();
     const reminderTime = new Date(time);
 
@@ -23,56 +100,49 @@ function setReminder(task, time) {
         document.getElementById('response').textContent = `Reminder: ${task}`;
         audio = new Audio(selectedSound);
         audio.play();
-
     }, timeDiff);
 }
 
-const recognition = new webkitSpeechRecognition(); // Create a speech recognition object
-recognition.continuous = false; // Disable continuous recognition
+const recognition = new webkitSpeechRecognition(); 
+recognition.continuous = false; 
 
-// Add an event listener for the "Talk" button to start speech recognition
 document.getElementById('submitBtn').addEventListener('click', function () {
-    // Start speech recognition when the "Talk" button is pressed
     recognition.start();
     document.getElementById('response').textContent = "Listening...";
 });
 
-// Handle speech recognition results
 recognition.onresult = function (event) {
-    const result = event.results[0][0].transcript.toLowerCase(); // Get the recognized speech in lowercase
-
-    // Map recognized words to your stored sound file names
+    const result = event.results[0][0].transcript.toLowerCase(); 
+    console.log('Recognized speech: ' + result);
     const soundMap = {
         "blue": "music/Black Eyed Peas - Pump It (Lyrics) - YouTube - Google Chrome 2023-11-02 15-56-21.mp3",
         "stop": "stop",
         "set": "music/iPhone Radar Alarm_Ringtone (Apple Sound) - Sound Effect for Editing - YouTube - Google Chrome 2023-11-02 17-06-09.mp3",
-        // Add more mappings for other sounds as needed
     };
 
-    if (soundMap[result]) {
+    if (Object.keys(soundMap).includes(result)) {
         const selectedSound = soundMap[result];
-        
-        if (selectedSound === "stop") {
-            // Stop the currently playing sound, if any
+
+        if (result === "stop") {
             if (audio) {
                 audio.pause();
                 audio.currentTime = 0;
                 document.getElementById('response').textContent = "Music stopped.";
             }
-        }
-        else if(selectedSound ==="set"){
-            const task = selectedSound;
-            const time = 10000;
-            setReminder(task, time)
-        }
-        else {
-            // Create an audio element to play the selected sound
+        } else if (result === "set") {
+            console.log("Set reminder");
+            populateCanvas(() => {
+
+                const task = selectedSound;
+                const time = Date.now() + 3000; 
+                setReminder(task, time, selectedSound);
+            });
+        } else {
+            console.log("Playing sound: " + selectedSound);
             audio = new Audio(selectedSound);
 
-            // Play the sound
             audio.play();
 
-            // Update the response div with a message
             document.getElementById('response').textContent = "Playing sound: " + selectedSound;
         }
     } else {
@@ -80,9 +150,6 @@ recognition.onresult = function (event) {
     }
 };
 
-// Handle speech recognition errors
 recognition.onerror = function (event) {
     console.error('Speech recognition error:', event.error);
 };
-
-
